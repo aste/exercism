d4c0147473f6d830789d574b71ba79b6ebcaee5c
Aste <aste@tuta.io>
2023-10-11 11:34:07 +0200

    Add first iteration of solution for simple Cipher

--- JavaScript/simple-cipher/simple-cipher.js
+++ JavaScript/simple-cipher/simple-cipher.js
@@ -3,20 +3,57 @@
 // convenience to get you started writing code faster.
 //
 
+import { text } from "node:stream/consumers";
+
 export class Cipher {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(key) {
+    this.initializeKey(key);
   }
 
-  encode() {
-    throw new Error('Remove this statement and implement this function');
+  static alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  _randomLetter() {
+    return Cipher.alphabet[Math.floor(Math.random() * 26)];
   }
 
-  decode() {
-    throw new Error('Remove this statement and implement this function');
+  initializeKey(value) {
+    if (typeof value == "string") {
+      this._key = value.toLowerCase();
+    } else {
+      this._key = "";
+      for (let i = 0; i < 100; i++) {
+        this._key += this._randomLetter();
+      }
+    }
   }
 
   get key() {
-    throw new Error('Remove this statement and implement this function');
+    return this._key;
+  }
+
+  encode(inputText) {
+    let encodedText = "";
+    let i = 0;
+    while (inputText[i] != undefined) {
+      let inputCharIndex = Cipher.alphabet.indexOf(inputText[i]);
+      let keyCharIndex = Cipher.alphabet.indexOf(this.key[i % this.key.length]);
+      let cipheredIndex = (inputCharIndex + keyCharIndex) % 26;
+      encodedText += Cipher.alphabet[cipheredIndex];
+      i += 1;
+    }
+    return encodedText;
+  }
+
+  decode(encodedText) {
+    let decodedText = "";
+    let i = 0;
+    while (encodedText[i] != undefined) {
+      let inputCharIndex = Cipher.alphabet.indexOf(encodedText[i]);
+      let keyCharIndex = Cipher.alphabet.indexOf(this.key[i % this.key.length]);
+      let cipheredIndex = (inputCharIndex - keyCharIndex + 26) % 26;
+      decodedText += Cipher.alphabet[cipheredIndex];
+      i += 1;
+    }
+    return decodedText;
   }
 }
