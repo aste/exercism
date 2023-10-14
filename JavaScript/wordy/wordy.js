b4f803097e7a457c492c69ed7c057ab63ac6e10c
Aste <aste@tuta.io>
2023-10-14 21:02:22 +0200

    My solution for wordy.js

--- JavaScript/wordy/wordy.js
+++ JavaScript/wordy/wordy.js
@@ -1,8 +1,41 @@
-//
-// This is only a SKELETON file for the 'Wordy' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const answer = (str) => {
+  const operators = ["-", "*", "+", "/"];
+  const valuesToRemove = ["What", "is", "by"];
 
-export const answer = () => {
-  throw new Error('Remove this statement and implement this function');
+  const numOprArr = str
+    .slice(0, -1)
+    .split(" ")
+    .filter((item) => !valuesToRemove.includes(item))
+    .map((element) => {
+      if (!isNaN(Number(element))) return Number(element);
+      else if (element === "plus") return "+";
+      else if (element === "minus") return "-";
+      else if (element === "multiplied") return "*";
+      else if (element === "divided") return "/";
+      else throw new Error("Unknown operation");
+    });
+
+  if (!numOprArr.length) throw new Error("Syntax error");
+
+  let result = numOprArr.shift();
+
+  while (numOprArr.length) {
+    let currentElement = numOprArr.shift();
+    let nextElement = numOprArr.shift();
+
+    if (
+      !operators.includes(currentElement) ||
+      typeof nextElement !== "number" ||
+      typeof result !== "number"
+    )
+      throw new Error("Syntax error");
+
+    if (currentElement === "+") result += nextElement;
+    else if (currentElement === "-") result -= nextElement;
+    else if (currentElement === "*") result *= nextElement;
+    else if (currentElement === "/") result /= nextElement;
+    else throw new Error("Syntax error");
+  }
+
+  return result;
 };
