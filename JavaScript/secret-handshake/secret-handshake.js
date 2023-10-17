7cd2b7439d9088620bf750bfbcd9cc4b5073fd1b
Aste <aste@tuta.io>
2023-10-17 16:45:28 +0200

    Add my initial solution for secret-handshake.js

--- JavaScript/secret-handshake/secret-handshake.js
+++ JavaScript/secret-handshake/secret-handshake.js
@@ -3,6 +3,31 @@
 // convenience to get you started writing code faster.
 //
 
-export const commands = () => {
-  throw new Error('Remove this statement and implement this function');
+export const commands = (givenNumber) => {
+  let secretHandshake = [];
+  let reverseHandshake = false;
+
+  const secretActions = {
+    0: "wink",
+    1: "double blink",
+    2: "close your eyes",
+    3: "jump",
+  };
+
+  // convert to binary using unsigned right shift
+  givenNumber = givenNumber.toString(2);
+
+  for (let i = givenNumber.length - 1, j = 0; i >= 0; i--, j++) {
+    if (givenNumber[i] === "1") {
+      if (j === 4) {
+        reverseHandshake = true;
+      } else {
+        secretHandshake.push(secretActions[j]);
+      }
+    }
+  }
+
+  if (reverseHandshake) secretHandshake.reverse();
+  console.log(secretHandshake);
+  return secretHandshake;
 };
